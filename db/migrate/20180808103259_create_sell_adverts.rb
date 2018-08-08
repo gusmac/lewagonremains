b75dc187230a9fd83e8fac603c9a7a8a10d2c726
Gus Macfarlane <gusmac@gmail.com>
2018-08-08 18:34:38 +0800

    sell advert, categories, subcategories

---
+++ db/migrate/20180808103259_create_sell_adverts.rb
@@ -0,0 +1,17 @@
+class CreateSellAdverts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :sell_adverts do |t|
+      t.string :title
+      t.text :description
+      t.references :user, foreign_key: true
+      t.monetize :price_cents
+      t.string :photo
+      t.string :address
+      t.string :condition
+      t.references :category, foreign_key: true
+      t.references :storage_space, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
