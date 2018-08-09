d1ad0e5c48fb087a6e7e06fdec74fba22c048a09
Julian Leopold <julian@rega-sense.ch>
2018-08-09 12:24:42 +0800

    pull

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,5 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :storage_space
   validates :start_date, :end_date, presence: true
-  monetize :optional_price_cents, allow_nil: true
+  monetize :price_cents, allow_nil: true
 end
