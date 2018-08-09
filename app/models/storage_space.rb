53fafbd72a32739a7f73c63d902ef9138b6585d5
Julian Leopold <julian@rega-sense.ch>
2018-08-09 19:32:24 +0800

    corrected validations

--- app/models/storage_space.rb
+++ app/models/storage_space.rb
@@ -2,7 +2,7 @@ class StorageSpace < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews
-
   validates :title, :description, :address, presence: true
   monetize :price_cents, allow_nil: true
+  # validates :photo, presence: true
 end
