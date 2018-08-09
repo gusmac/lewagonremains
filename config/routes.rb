f666385c10b15963c84fb78f5cafc8d3d43efaf3
KevinJudith <39331879+KevinJudith@users.noreply.github.com>
2018-08-09 19:09:00 +0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'storage_spaces/index'
-  get 'storage_spaces/new'
-  get 'storage_spaces/create'
-  get 'storage_spaces/show'
-  get 'storage_spaces/update'
-  get 'storage_spaces/destroy'
-  get 'storage_spaces/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
