4c84d40154534d190718f6931e6e534b98150a19
Gus Macfarlane <gusmac@gmail.com>
2018-08-10 09:49:21 +0800

    added username instead of name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_10_013119) do
+ActiveRecord::Schema.define(version: 2018_08_10_014720) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -339,8 +339,9 @@ ActiveRecord::Schema.define(version: 2018_08_10_013119) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "phone_number"
-    t.string "name"
+    t.string "user"
     t.boolean "admin", default: false, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
