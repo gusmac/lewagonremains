7a56319482046dd0368dddc1726e2d4062842b4e
Julian <36457814+JLeop@users.noreply.github.com>
2018-08-11 20:08:05 +0800

    Update bookings_controller.rb

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -22,10 +22,10 @@ class BookingsController < ApplicationController
     @booking.storage_space = @storage_space
 
     # # validates dates
-    # if @booking.start_date < @booking.end_date || @booking.start_date < Date.today
-    #   raise # raise an error. Does not even get to saving.
-    #   # TODO maybe we find a smart gem to do this.
-    # end
+    if @booking.start_date < @booking.end_date || @booking.start_date < Date.today
+      raise # raise an error. Does not even get to saving.
+      # TODO maybe we find a smart gem to do this.
+    end
 
     # calculating the price
     # number_of_days = @booking.end_date - @booking.start_date
