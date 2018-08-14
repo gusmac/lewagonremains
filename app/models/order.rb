01f3f083889624aede816a4106fe03fd74c443ae
Gus Macfarlane <gusmac@gmail.com>
2018-08-14 11:29:43 +0800

    added status to orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,7 @@
 class Order < ApplicationRecord
   belongs_to :sell_advert
   belongs_to :user
+
+  validates :status, inclusion: { in: ["Pending", "Cancelled", "Confirmed"] }
+  validates :user, :sell_advert, presence: true
 end
