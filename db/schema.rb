f7d3a78f2d0f6cd97f44e2470cfcae29dcedf0b9
Julian Leopold <julian@rega-sense.ch>
2018-08-15 18:28:28 +0800

    reviews on show of storage space

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_15_034125) do
+ActiveRecord::Schema.define(version: 2018_08_15_073339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
