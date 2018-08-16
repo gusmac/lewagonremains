6816dae4db7fede37df78d08a2b1f7ae8fe40282
Gus Macfarlane <gusmac@gmail.com>
2018-08-16 14:17:47 +0800

    link to fake post established

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get '/storer', to: 'pages#storer'
-  get '/forumtest', to: 'pages#forum'
-  get '/forumtestpost', to: 'pages#forum_post'
+  get '/forum', to: 'pages#forum'
+  get '/forumpost', to: 'pages#forum_post'
   get '/browse_wanted', to: 'buy_adverts#browse_buy'
   get '/browse_for_sale', to: 'sell_adverts#browse_sell'
   get '/browse_storage_space', to: 'storage_spaces#browse_storage_space'
-  mount Thredded::Engine => '/forum'
+  # mount Thredded::Engine => '/forum'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'user/dashboard', to: 'users#dashboard', as: 'user_dashboard'
