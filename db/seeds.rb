64f77cf9fe64d3e305653ed9201881f6678807bd
Julian Leopold <julian@rega-sense.ch>
2018-08-16 17:56:17 +0800

    merging

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ User.find_by(email: 'test@test.com').update(admin: true)
 User.find_by(email: 'test@test.com').update(username: "test")
 
 # users
-dirk = User.create!(email: "dirk@lewagon.com", password: "testtest", first_name: "Dirk", last_name: "Schuler")
+dirk = User.create!(email: "dirk@lewagon.com", password: "testtest", first_name: "Dirk", last_name: "Schuler", username: "Dirk")
 julian = User.create!(email: "julian@rega-sense.ch", password: "testtest", first_name: "Julian", last_name: "Leopold")
 kevin = User.create!(email: "kjsmekens@gmail.com", password: "testtest", first_name: "Kevin", last_name: "Smekens")
 gus = User.create!(email: "gusmac@gmail.com", password: "testtest", first_name: "Gus", last_name: "Macfarlnae")
@@ -32,7 +32,7 @@ gus = User.create!(email: "gusmac@gmail.com", password: "testtest", first_name: 
 # random users
 random_users = []
 10.times do
-  random_users << User.create!(email: Faker::Internet.email, password: "testest", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  random_users << User.create!(email: Faker::Internet.email, password: "testest", first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, username: Faker::Name.first_name)
 end
 
 puts "categories"
@@ -49,22 +49,41 @@ bikes = Subcategory.create!(category: sports, name: "Bicycles")
 office = Subcategory.create!(category: furniture, name: "Office")
 apple = Subcategory.create!(category: computers, name: "Apple")
 
-puts " storage spaces"
-titles = ["Empty Garage", "Locked and secured barn", "empty room in my appartment", "10 square meters free storage space"]
-descriptions = ["Dry, locked and a lot of space", "Room for 2 cars", "Can store anything you want. ATTENTION: My appartment is on the first floor", "Space or a couple surfboards or like."]
-prices = [5, 10, 15, 7]
-photos = ["https://res.cloudinary.com/j163surf77/image/upload/v1533895555/garage.jpg", "https://res.cloudinary.com/j163surf77/image/upload/v1533895557/barn.jpg", "https://res.cloudinary.com/j163surf77/image/upload/v1533895553/empty_room.jpg", "https://res.cloudinary.com/j163surf77/image/upload/v1533629338/vkaauqzqdlrolcs723bg.jpg"]
-addresses = ["Batu Bolong, Canggu", "Berawa Beach, North Kuta", "Seminyak, North Kuta, Bali", "Ubud, Bali" ]
+puts "storage spaces"
+titles = ["Big Open Empty Garage", "Locked and Secured Barn", "Empty Room perfect for Surfboards"]
+description = "I am renting out a room in my flat as I don't need it. The place is locked at night and barely left alone for longer than a day.
+                The temperature is controlled (room temperature).
+                 <ul>
+                  <li>30 cubic meters</li>
+                  <li>Perfect for: Boards, Instruments, Equipment</li>
+                  <li>locked whenever noone is home</li>
+                </ul>
+                Just message me if you have questions or simply book! I am available on Whatsapp 7/7"
+prices = [470, 300, 200]
+photos = ["https://res.cloudinary.com/j163surf77/image/upload/v1534401042/roof.jpg",
+  "https://res.cloudinary.com/j163surf77/image/upload/v1534401066/bali_green_house_entry.jpg",
+  "http://res.cloudinary.com/j163surf77/image/upload/v1534401667/egg_house.jpg"]
+addresses = ["Batu Bolong, Canggu", "Berawa, North Kuta", "Ubud, Bali"]
 
 titles.each_with_index do |title, index|
-  StorageSpace.create!(user: random_users.sample, title: title , description: descriptions[index], price_cents: prices[index], remote_photo_url: photos[index], address: addresses[index])
+  StorageSpace.create!(user: random_users.sample, title: title , description: description, price_cents: prices[index], remote_photo_url: photos[index], address: addresses[index])
 end
 
+dirks_space = StorageSpace.create!(user: dirk, title: "10 Square Meters Free Storage Space", description: description, price_cents: 250, remote_photo_url: "https://res.cloudinary.com/j163surf77/image/upload/v1534401073/bali_room.jpg", address: "Seminyak, North Kuta")
+
+
+
 puts "sell adverts"
-items = ["Bicycle", "MacBook Pro 13'", "Scooter Helmet", "Almost New Motorbike Helmet", "6.8 Surfboard Snapped only once."]
-description = ["Fantastic Racer. I bought the bike at the beginning of my the bootcamp and can't take it home.", "After my Lenovo would not install Linux I bought this computer. Since my company is giving me a new one I do not need it anymore.", "Bought here in Bali to have some decent protection. Couple scrates but otherwise in good condition. New price was 1.2 Mil IDR", "If you want to protect your head INVEST in a real helmet! Seriously, most people here can't drive. And dont drive drunk!!", "Bought a new surfboard, broke it and repaired it."]
-photos = ["https://res.cloudinary.com/j163surf77/image/upload/v1533895558/bicycle.jpg", "https://res.cloudinary.com/j163surf77/image/upload/v1533895553/macbookpro.jpg", "https://res.cloudinary.com/j163surf77/image/upload/v1533895556/helmet.jpg", "https://res.cloudinary.com/j163surf77/image/upload/v1533895556/helmet.jpg", "https://res.cloudinary.com/j163surf77/image/upload/v1534131736/snapped_board.jpg"]
-prices = [300, 1000, 40, 200, 200.00]
+items = ["Bicycle", "MacBook Pro 13'", "Scooter Helmet", "Performance Shortboard"]
+description = ["Fantastic Racer. I bought the bike at the beginning of my the bootcamp and can't take it home.",
+  "After my Lenovo would not install Linux I bought this computer. Since my company is giving me a new one I do not need it anymore.",
+  "Bought here in Bali to have some decent protection. Couple scrates but otherwise in good condition. New price was 1.2 Mil IDR",
+  "Bought a new surfboard, broke it and repaired it."]
+photos = ["https://res.cloudinary.com/j163surf77/image/upload/v1534401057/bicycle.jpg",
+  "https://res.cloudinary.com/j163surf77/image/upload/v1533895553/macbookpro.jpg",
+  "https://res.cloudinary.com/j163surf77/image/upload/v1533895556/helmet.jpg",
+  "https://res.cloudinary.com/j163surf77/image/upload/v1534131736/shortboard.jpg"]
+prices = [30000, 100000, 4000, 20000, 20000]
 conditions = ["Used", "Like-New", "Used", "Like-New", "Used"]
 subcategories = [bikes, apple, gear, gear, surfboards]
 categories = [sports, computers, sports, sports, sports]
@@ -79,34 +98,35 @@ items.each_with_index do |item, index|
 end
 
 puts "buy adverts"
-titles = ["Looking for a Skateboard", "Protective gear for scooter", "Scuba gear", "Searching cheap MacBook for bootcamp" ]
+titles = ["Protective gear for scooter", "Scuba gear", "Searching cheap MacBook for bootcamp" ]
 descriptions = ["Skate or longboard to commute to boot camp", "Trying to protect myself while crusing around. Looking for gloves, helmet, and jacket", "Being an avid scooba diver I want to use the proximity to the ocean. Unfortunately I don't have my gear with me.", "I read in the forum that I should get a mac for the bootcamp."]
-prices = [50, 200, 300, 600]
-photos = ["", "", "https://res.cloudinary.com/j163surf77/image/upload/v1533895555/scuba_diver.jpg", ""]
-subcategories = [surfboards, gear, gear, apple]
-categories = [sports, sports, sports, computers]
+prices = [20000, 30000, 60000]
+photos = ["", "https://res.cloudinary.com/j163surf77/image/upload/v1533895555/scuba_diver.jpg",
+  "https://res.cloudinary.com/j163surf77/image/upload/v1534402398/macbookcorner.jpg"]
+subcategories = [gear, gear, apple]
+categories = [sports, sports, computers]
 titles.each_with_index do |title, index|
   b = BuyAdvert.new(user: random_users.sample, title: title , description: descriptions[index], price_cents: prices[index], category: categories[index], subcategory: subcategories[index], remote_photo_url: photos[index])
   b.save!
 end
 
-puts "bookings"
-comments = ["Please be very careful with my things", "Will be picked up by Jack as discussed", "Make sure it does not get wet", "", "", "Thank you very much in advance!"]
-start_dates = [Date.today-2, Date.today-6, Date.today-8, Date.today-10, Date.today-13]
-end_dates = [Date.today+1, Date.today+4, Date.today+7, Date.today+10, Date.today+15]
-status = ["Confirmed", "Pending", "Confirmed", "Pending", "Confirmed", "Pending", "Cancelled"]
-10.times do
-  Booking.create!(user: random_users.sample, storage_space: StorageSpace.all.sample, comment: comments.sample, start_date: start_dates.sample, end_date: end_dates.sample , status: status.sample)
-end
+# puts "bookings"
+# comments = ["Please be very careful with my things", "Will be picked up by Jack as discussed", "Make sure it does not get wet", "", "", "Thank you very much in advance!"]
+# start_dates = [Date.today-2, Date.today-6, Date.today-8, Date.today-10, Date.today-13]
+# end_dates = [Date.today+1, Date.today+4, Date.today+7, Date.today+10, Date.today+15]
+# status = ["Confirmed", "Pending", "Confirmed", "Pending", "Confirmed", "Pending", "Cancelled"]
+# 10.times do
+#   Booking.create!(user: random_users.sample, storage_space: StorageSpace.all.sample, comment: comments.sample, start_date: start_dates.sample, end_date: end_dates.sample , status: status.sample)
+# end
 
 puts "reviews"
-descriptions = ["Nice. Stored in a clean cupboard.", "Took really good care of my stuff", "Great guy!", "I rented a unit with Extra Space Storage. When it rained the roof leaked. ", "The temperature in the storage unit was appropriate. I will use their service in a heartbeat.", "These guys get you in with “specials” and then raise rates as fast as they can.", "Great and super friendly service! They explained prices and terms very clearly.", "Been renting a 10x10 storage unit for five months and paid on time. 6th month", "Our stuff were exactly as we left them and we didn't lose anything.","Extra Space raises my rent 100% without notifying me and deducts it from my charge card."]
+descriptions = ["Nice. Stored in a clean cupboard.", "Took really good care of my stuff", "Great guy!", "I rented a unit with Extra Space Storage. When it rained the roof leaked. ", "The temperature in the storage unit was appropriate.", "These guys get you in with “specials” and then raise rates as fast as they can.", "Great and super friendly service! They explained prices and terms very clearly.", "Been renting a 10x10 storage unit for five months. Satisfied", "Our stuff were exactly as we left them and we didn't lose anything.","Extra Space raises my rent 100% without notifying me and deducts it from my charge card."]
 
 StorageSpace.all.each do |space|
-  number_of_reviews = (3..10  ).to_a.sample
+  number_of_reviews = (8).to_a.sample
   number_of_reviews.times do
-    r = Review.new(rating: [5,5,5,4,4,4,3,3,2,1].sample, description: descriptions.sample, storage_space: space)
-    r.user = User.all.sample
+    r = Review.new(rating: [5,5,5,4,4,4].sample, description: descriptions.sample, storage_space: space)
+    r.user = random_users.sample
     r.save!
   end
 end
