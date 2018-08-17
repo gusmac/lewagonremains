76f07b9a9ebc5c19bfe9a2fc02214886d011a3d5
Julian Leopold <julian@rega-sense.ch>
2018-08-17 15:04:13 +0800

    form not perfect yet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,9 +5,11 @@ import { initUpdateNavbarOnScroll } from '../components/navbar.js';
 import { initDropdownClick } from './dropdown';
 import { categorySelector } from './categoryselector';
 import { bindSweetAlertButtonDemo } from '../components/sweetalert';
+import { initFlatpickr } from './flatpickr'
 bindSweetAlertButtonDemo();
 initUpdateNavbarOnScroll();
 initDropdownClick();
 window.$ = $;
 window.jQuery = $;
 categorySelector();
+initFlatpickr();
