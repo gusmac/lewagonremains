ba9e3e6331f6c422e5c36b17147adb55d695506f
Kevin Smekens <kjsmekens@gmail.com>
2018-08-17 10:47:14 +0800

    added sweetalert buy button

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,11 @@ import "./subcategories";
 import $ from 'jquery';
 import { initUpdateNavbarOnScroll } from '../components/navbar.js';
 import { initDropdownClick } from './dropdown';
+import { bindSweetAlertButtonDemo } from '../components/sweetalert';
+bindSweetAlertButtonDemo();
 initUpdateNavbarOnScroll();
 initDropdownClick();
 window.$ = $;
 window.jQuery = $;
+
+
